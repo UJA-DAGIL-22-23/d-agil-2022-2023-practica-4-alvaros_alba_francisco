cd1447e35203a345a336719d3bb5f18b0697da19
Álvaro Martín Bacas <amb00093@red.ujaen.es>
2023-04-25 12:51:23 +0200

    Fichero callbacks.js de Volley actualizado

--- ms-voleibol/callbacks.js
+++ ms-voleibol/callbacks.js
@@ -3,9 +3,10 @@
  * @description Callbacks para el MS Plantilla.
  * Los callbacks son las funciones que se llaman cada vez que se recibe una petición a través de la API.
  * Las peticiones se reciben en las rutas definidas en routes.js, pero se procesan aquí.
- * @author Alba Gómez Liébana <agl00108@red.ujaen.es>
- * @date 07-mar-2023
+ * @author Víctor M. Rivas <vrivas@ujaen.es>
+ * @date 03-feb-2023
  */
+
 
 
 // Necesario para conectar a la BBDD faunadb
@@ -13,10 +14,10 @@ const faunadb = require('faunadb'),
     q = faunadb.query;
 
 const client = new faunadb.Client({
-    secret: 'fnAE-YylRgAAzRLy7JPO8A04vyLO5ht55mI4WfDO',
+    secret: 'fnAFAFQ6zuAAzdCbQmODooigiRDvcD3a1ApmPCfD',
 });
 
-const COLLECTION = "Deportistas"
+const COLLECTION = "Volley"
 
 // CALLBACKS DEL MODELO
 
@@ -42,72 +43,83 @@ function CORS(res) {
  */
 const CB_MODEL_SELECTS = {
     /**
-     * Prueba de conexión a la BBDD: devuelve todas los deportistas que haya en la BBDD.
+     * Prueba de conexión a la BBDD: devuelve todas las jugadors que haya en la BBDD.
      * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
      * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
      */
     test_db: async (req, res) => {
         try {
-            let deportistas = await client.query(
+            let plantilla = await client.query(
                 q.Map(
                     q.Paginate(q.Documents(q.Collection(COLLECTION))),
                     q.Lambda("X", q.Get(q.Var("X")))
                 )
             )
-            res.status(200).json(deportistas)
+            res.status(200).json(plantilla)
         } catch (error) {
             res.status(500).json({ error: error.description })
         }
     },
-
-    /**
-     * Método para obtener todas los deportistas de la BBDD.
-     * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
-     * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
-     */
-    getTodas: async (req, res) => {
+    getTodos: async (req, res) => {
         try {
-            let deportistas = await client.query(
+            let plantilla = await client.query(
                 q.Map(
                     q.Paginate(q.Documents(q.Collection(COLLECTION))),
                     q.Lambda("X", q.Get(q.Var("X")))
                 )
             )
+            //console.log( plantilla ) // Para comprobar qué se ha devuelto en proyectos
             CORS(res)
                 .status(200)
-                .json(deportistas)
+                .json(plantilla)
         } catch (error) {
             CORS(res).status(500).json({ error: error.description })
         }
     },
-
-    /**
-    * Método para cambiar los datos de un deportista
-    * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
-    * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
-    */
-    setCampos: async (req, res) => 
-    {
-        try 
-        {
+    getPorId: async (req, res) => {
+        try {
+            // console.log( "getPorId req", req.params.idjugador ) // req.params contiene todos los parámetros de la llamada
+            let jugador1 = await client.query(
+                q.Get(q.Ref(q.Collection(COLLECTION), req.params.idJugador))
+            )
+            // console.log( jugador ) // Para comprobar qué se ha devuelto en jugador
+            CORS(res)
+                .status(200)
+                .json(jugador1)
+        } catch (error) {
+            CORS(res).status(500).json({ error: error.description })
+        }
+    },
+    setTodo: async (req, res) => {
+        console.log("setTodo req.body", req) // req.body contiene todos los parámetros de la llamada
+        try {
             let valorDevuelto = {}
+            // Hay que comprobar Object.keys(req.body).length para saber si req.body es objeto "normal" o con problemas
+            // Cuando la llamada viene de un formulario, se crea una sola entrada, con toda la info en una sola key y el value está vacío.
+            // Cuando la llamada se hace con un objeto (como se hace desde el server-spec.js), el value No está vacío.
             let data = (Object.values(req.body)[0] === '') ? JSON.parse(Object.keys(req.body)[0]) : req.body
-            let deportista = await client.query(
+            console.log("SETTODO data es", data)
+            let jugador = await client.query(
                 q.Update(
-                    q.Ref(q.Collection(COLLECTION), data.id_deportista),
+                    q.Ref(q.Collection(COLLECTION), data.id_jug),
+                    
                     {
-                        data: 
-                        {
-                            nombre: data.nombre_deportista,
-                            apellidos: data.apellidos_deportista,
-                            pais: data.pais_deportista,
-                            categoria: data.categoria_deportista,
+                        data: {
+                            nombre: data.nombre_jug,
+                            apellidos: data.apellidos_jug,
+                            nacimiento: data.nac_jug,
+                            pais_nacimiento: data.pais_jug,
+                            participacionesMundial: data.part_jug,
+                            numParticipaciones: data.numPart_jug,
+                            club_actual: data.club_jug,
+                            posicion: data.posicion_jug,
                         },
                     },
                 )
             )
                 .then((ret) => {
                     valorDevuelto = ret
+                    //console.log("Valor devuelto ", valorDevuelto)
                     CORS(res)
                         .status(200)
                         .header( 'Content-Type', 'application/json' )
@@ -118,77 +130,17 @@ const CB_MODEL_SELECTS = {
             CORS(res).status(500).json({ error: error.description })
         }
     },
-
-    /**
-    * Método para agregar un nuevo deportista
-    * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
-    * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
-    */
-    setNuevoDeportista: async (req, res) => 
-    {
-        try 
-        {
-          let valorDevuelto = {}
-          let data = (Object.values(req.body)[0] === '') ? JSON.parse(Object.keys(req.body)[0]) : req.body
-          let nuevoDeportista = await client.query(
-            q.Create(
-              q.Collection(COLLECTION),
-              {
-                data: {
-                    nombre: data.nombre_deportista,
-                    apellidos: data.apellidos_deportista,
-                    fechaNacimiento: data.fechaNacimiento_deportista,
-                    aniosParticipacionOlimpiadas: data.aniosParticipacionOlimpiadas_deportista,
-                    numMedallasGanadas: data.numMedallasGanadas_deportista,
-                    logros: data.logros_deportista,
-                    pais: data.pais_deportista,
-                    categoria: data.categoria_deportista,
-                    sexo: data.sexo_deportista
-                  },
-                },
-            )
-            )
-            .then((ret) => {
-                valorDevuelto = ret
-                CORS(res)
-                    .status(200)
-                    .header( 'Content-Type', 'application/json' )
-                    .json(valorDevuelto)
-            })
-        
-        } catch (error) 
-        {
-          console.log(error);
-          CORS(res).status(500).json({ error: error.description });
-        }
-      },
-      
-
-    /**
-     * Método para obtener una persona de la BBDD por su id.
-     * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
-     * @param {*} res Objeto Response con las respuesta que se va a dar a la petición recibida
-     */
-    getPorId: async (req, res) => {
-        try {
-            let deportista = await client.query(
-                q.Get(q.Ref(q.Collection(COLLECTION), req.params.idDeportista))
-            )
-            CORS(res)
-                .status(200)
-                .json(deportista)
-        } catch (error) {
-            CORS(res).status(500).json({ error: error.description })
-        }
-        },
 }
 
+
+
+
 // CALLBACKS ADICIONALES
+
 /**
  * Callbacks adicionales. Fundamentalmente para comprobar que el ms funciona.
  */
-const CB_OTHERS = 
-{
+const CB_OTHERS = {
     /**
      * Devuelve un mensaje indicando que se ha accedido a la home del microservicio
      * @param {*} req Objeto con los parámetros que se han pasado en la llamada a esta URL 
@@ -210,15 +162,16 @@ const CB_OTHERS =
     acercaDe: async (req, res) => {
         try {
             CORS(res).status(200).json({
-                mensaje: "Datos de la autora",
-                autor: "Alba Gómez Liébana",
-                email: "agl00108@red.ujaen.es",
-                fecha: "07/03/2023"
+                mensaje: "Microservicio MS Plantilla: acerca de",
+                autor: "Álvaro Martín",
+                email: "amb00093@red.ujaen.es",
+                fecha: "28/03/2023"
             });
         } catch (error) {
             CORS(res).status(500).json({ error: error.description })
         }
     },
+
 }
 
 // Une todos los callbacks en un solo objeto para poder exportarlos.
