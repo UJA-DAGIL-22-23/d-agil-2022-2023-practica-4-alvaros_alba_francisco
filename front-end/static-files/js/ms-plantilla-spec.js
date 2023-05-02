b01e44c19fe80c9fdb7ea061a58d84154d7bfc83
agl00108 <agl00108@red.ujaen.es>
2023-05-02 13:02:13 +0200

    Spec para pie tabla y datos

--- front-end/static-files/js/ms-plantilla-spec.js
+++ front-end/static-files/js/ms-plantilla-spec.js
@@ -18,7 +18,58 @@ describe("Plantilla.cabeceraTable():", function ()
     it("debería devolver las etiquetas HTML para la cabecera de la tabla",
         function () 
         {
-            expect(Halterofilia.cabeceraTableNombre()).toBe(`<table class="listado-nombres"><thead><th>Deporte</th><th>Nombre</th><th>Apellidos</th></thead><tbody>`);
+            expect(Plantilla.cabeceraTable()).toBe(`<table class="listado-nombres"><thead><th>Deporte</th><th>Nombre</th><th>Apellidos</th></thead><tbody>`);
         })
 })
+
+describe("Plantilla.cuerpoTr():", function () 
+{
+    it("debería devolver una fila de tabla con los nombres de los deportistas",
+        function () 
+        {
+            //Definimos las constantes que vamos a usar
+            let vector = {}
+            vector.data = [
+                {
+                    ref: {
+                        "@ref": {
+                            id: "Id de Luis"
+                        }
+                    },
+                    data: {
+                        nombre: "Luis",
+                        apellidos: "Martinez"
+                    }
+                },
+                {
+                    ref: {
+                        "@ref": {
+                            id: "Id de Ana"
+                        }
+                    },
+                    data: {
+                        nombre: "Ana",
+                        apellidos: "Rafael"
+                    }
+                }
+            ]
+            for (let i = 0; i < vector.data.length; ++i) 
+            {
+                let msj = Plantilla.cuerpoTr(vector.data[i])
+                let persona = vector.data[i]
+                expect(msj.includes(persona.ref["@ref"].id)).toBeTrue();
+                expect(msj.includes(persona.data.nombre)).toBeTrue();
+            }
+        })
+})
+
+describe("Plantilla.pieTable():", function () 
+{
+    it("debería devolver las etiquetas HTML para el pie de tabla",
+        function () 
+        {
+            expect(Plantilla.pieTable()).toBe("</tbody></table>");
+        })
+})
+
 
--- front-end/static-files/js/ms-plantilla.js
+++ front-end/static-files/js/ms-plantilla.js
@@ -18,3 +18,27 @@ Plantilla.cabeceraTable = function () {
     return `<table class="listado-nombres"><thead><th>Deporte</th><th>Nombre</th><th>Apellidos</th></thead><tbody>`;
 }
 
+/**
+ * Muestra el nombre de cada deportista en un elemento TR con sus correspondientes TD
+ * @param {deportista} deportista Datos del deportista a mostrar con los nombres de los deportistas
+ * @returns Cadena conteniendo todo el elemento TR que muestra los nombres.
+ */
+Plantilla.cuerpoTr = function (p, deporte) {
+    const d = p.data
+
+    return `<tr title="${p.ref['@ref'].id}">
+    <td><em>${deporte}</em></td>
+    <td><em>${d.nombre}</em></td>
+    <td><em>${d.apellidos}</em></td>
+    </tr>
+    `;
+}
+
+/**
+ * Muestra el pie de tabla de los nombres de los deportistas
+ * @returns Cadena conteniendo el pie de tabla
+ */
+Plantilla.pieTable = function () {
+    return "</tbody></table>";
+}
+
