0b191dae246d2ad4a114f47c0335a8881f6b7a0f
DESKTOP-FIE3BLF\Álvaro <aha00026@red.ujaen.es>
2023-05-06 17:51:01 +0200

    Añado las correspondientes pruebas TDD de surferos

--- api-gateway/spec/server-spec.js
+++ api-gateway/spec/server-spec.js
@@ -335,3 +335,34 @@ describe('API Gateway: rutas estáticas', () => {
 
   })
 });
+
+describe('API Gateway: rutas estáticas', () => {
+  describe('Rutas estáticas de MS Plantilla', () => {
+    it('Devuelve MS Plantilla Home Page', (done) => {
+      supertest(app)
+        .get('/surferos/')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.hasOwnProperty('mensaje'));
+          assert(res.body.mensaje === "Microservicio MS Plantilla: home");
+
+        })
+        .end((error) => { error ? done.fail(error) : done() })
+    });
+    it('Devuelve MS Plantilla Acerca De', (done) => {
+      supertest(app)
+        .get('/surferos/acercade')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function (res) {
+          //console.log( "BODY ACERCA DE ", res.body ); // Para comprobar qué contiene exactamente res.body
+          assert(res.body.hasOwnProperty('mensaje'));
+          assert(res.body.mensaje === "Microservicio MS Plantilla: acerca de");
+
+        })
+        .end((error) => { error ? done.fail(error) : done() })
+    });
+  })
+});
